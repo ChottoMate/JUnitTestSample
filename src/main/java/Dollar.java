76265a03c4dd15802a7f87399fb8229b06c1a3ae
ChottoMatte <jin92370@gmail.com>
2022-06-01 23:21:22 +0900

    Initial commit

---
+++ src/main/java/Dollar.java
@@ -0,0 +1,12 @@
+public class Dollar extends Money {
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+    Money times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+//    public boolean equals(Object object) {
+//        Money money = (Money) object;
+//        return amount == money.amount;
+//    }
+}
