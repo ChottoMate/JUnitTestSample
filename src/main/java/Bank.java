e87ee6466dee96fdf6d8badc143325cab7bdb879
ChottoMatte <jin92370@gmail.com>
2022-06-03 10:23:51 +0900

    13章

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,5 +1,11 @@
 public class Bank {
     Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+//        if (source instanceof Money) {
+//            return ((Money) source).reduce(to);
+//        }
+//        Sum sum = (Sum) source;
+        return source.reduce(to);
+//        int amount = sum.augend.amount + sum.addend.amount;
+//        return new Money(amount, to);
     }
 }
