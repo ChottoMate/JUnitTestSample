856f2930b2956a50454ab0a20b7ff3b0de6311b1
ChottoMatte <jin92370@gmail.com>
2022-06-04 00:04:28 +0900

    14章

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,11 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+    private Map<Pair, Integer> rates = new HashMap<>();
+
     Money reduce(Expression source, String to) {
 //        if (source instanceof Money) {
 //            return ((Money) source).reduce(to);
 //        }
 //        Sum sum = (Sum) source;
-        return source.reduce(to);
+        return source.reduce(this, to);
 //        int amount = sum.augend.amount + sum.addend.amount;
 //        return new Money(amount, to);
+    }
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
+    }
+    int rate(String from, String to) {
+        if (from.equals(to)) return 1;
+        int rate = rates.get(new Pair(from, to));
+        return rate;
     }
 }
